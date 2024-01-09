f488f5db0a50e0b6f95549b74ba7678b50738801
sgyanendra <154693868+sgyanendra@users.noreply.github.com>
2024-01-09 19:37:19 +0000

    spiral

---
+++ spiral.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class spiral{
+
+    public static void printArr(int num[][]){
+        int startRow=0;
+        int startCol=0;
+        int endRow=num.length-1;
+        int endCol=num.length-1;
+
+        for(int i=startRow;i<=endRow;i++){
+          System.out.print(num[i][startCol]+" ");
+        }
+        for(int i=startCol+1;i<=endCol;i++){
+          System.out.print(num[startRow][i]+" ");
+        }
+        for(int i=endCol-1;i>=startCol;i--){
+          System.out.print(num[endRow][i]+" ");
+        }
+         for(int i=endRow-1;i>=startRow+1;i--){
+          System.out.print(num[i][startCol]+" ");
+        }
+        startRow++;
+        startCol++;
+        endCol--;
+        endRow--;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num[][]=new int[4][4];
+        System.out.println("Enter the Number!!");
+        for(int i=0;i<4;i++){
+          for(int j=0;j<4;j++){
+            num[i][j]=sc.nextInt();
+          }
+        }
+       printArr(num);
+    }
+}
