e105a261bd795fc1e425676b2198e440f0cf53a7
sgyanendra <154693868+sgyanendra@users.noreply.github.com>
2024-01-09 18:04:27 +0000

    sort

--- sort.java
+++ sort.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class sort{
     
     public static void bubbol(int num[]){
@@ -37,16 +38,38 @@ class sort{
         num[prev+1]=cur;
      }
     }
+
+    public static void counting(int num[]){
+       int largest=Integer.MIN_VALUE;
+       for(int i=0;i<num.length;i++){
+           largest=Math.max(largest,num[i]);
+       }
+
+       int count[]=new int[largest+1];
+       for(int i=0;i<num.length;i++){
+         count[num[i]]++;
+       }
+
+       int j=0;
+       for(int i=0;i<count.length;i++){
+          while(count[i]>0){
+             num[j]=i;
+             j++;
+             count[i]--;
+          }
+       }
+    }
     public static void printArr(int num[]){
         for(int i=0;i<num.length;i++){
            System.out.print(num[i]);
         }
     }
     public static void main(String arg[]){
-     int num[]={2,7,5,9,1,3};
+     int num[]={2,7,5,9,1,3,1,2,5};
      //bubbol(num);
      //selection(num);
-     insertion(num);
+     //insertion(num);
+     counting(num);
      printArr(num);
     }
 }
