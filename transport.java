d6d15dcca973635d7a15b3ca09df8c14ed95fa95
sgyanendra <154693868+sgyanendra@users.noreply.github.com>
2024-01-10 18:39:50 +0000

    transport

---
+++ transport.java
@@ -0,0 +1,12 @@
+class transport{
+
+    public static void main(String ar[]){
+      int num[][]={{1,2,3},{4,5,6}};
+      for(int i=0;i<3;i++){
+         for(int j=0;j<2;j++){
+            System.out.print(num[j][i]);
+         } 
+         System.out.println();
+      }
+    }
+}
